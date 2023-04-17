f918dbba6176f595c86d563bed355d1859fdff75
Maximilian Georg Kurzawski <maximilian.kurzawski@gmail.com>
2023-04-17 12:32:55 +0200

    init repo add file for exercise1

---
+++ assignment2/exercise1/q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        printf("Hello World from Thread %d!\n", thread_id);
+    }
+    return 0;
+}
+
+
