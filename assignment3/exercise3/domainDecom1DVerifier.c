68b6fa7a00577f91c3d7cde83ad17a45631dcf83
MKLepium <maximilian.kurzawski@gmail.com>
2023-05-12 12:19:07 +0200

    Added all parts for exercise3

---
+++ assignment3/exercise3/domainDecom1DVerifier.c
@@ -0,0 +1,71 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size, i, provided;
+
+    // number of cells (global)
+    int nxc = 128; // make sure nxc is divisible by size
+    double L = 2 * 3.1415; // Length of the domain
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // number of nodes (local to the process): 0 and nxn_loc-1 are ghost cells
+    int nxn_loc = nxc / size + 3; // number of nodes is number cells + 1; we add also 2 ghost cells
+    double L_loc = L / ((double)size);
+    double dx = L / ((double)nxc);
+
+    // define our function
+    double *f = calloc(nxn_loc, sizeof(double)); // allocate and fill with z
+    double *dfdx = calloc(nxn_loc, sizeof(double)); // allocate and fill with z
+
+    for (i = 1; i < (nxn_loc - 1); i++)
+        f[i] = sin(L_loc * rank + (i - 1) * dx);
+
+    // need to communicate and fill ghost cells f[0] and f[nxn_loc-1]
+
+    // Blocking communication
+    MPI_Status status;
+
+    // Left cell
+    int left = (rank - 1 + size) % size;
+    // Right cell
+    int right = (rank + 1) % size;
+
+    // Send left ghost cell and receive into the right ghost cell
+    MPI_Sendrecv(&f[1], 1, MPI_DOUBLE, left, 0, &f[nxn_loc - 1], 1, MPI_DOUBLE, right, 0, MPI_COMM_WORLD, &status);
+
+    // Send right ghost cell and receive into the left ghost cell
+    MPI_Sendrecv(&f[nxn_loc - 2], 1, MPI_DOUBLE, right, 1, &f[0], 1, MPI_DOUBLE, left, 1, MPI_COMM_WORLD, &status);
+
+    // Gather all values of f from all processes to rank 0
+    double *all_f = NULL;
+    if (rank == 0) {
+        all_f = malloc(nxn_loc * size * sizeof(double));
+    }
+    MPI_Gather(f, nxn_loc, MPI_DOUBLE, all_f, nxn_loc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Print f values on rank 0
+    if (rank == 0) {
+        printf("My rank %d of %d\n", rank, size);
+        printf("Here are my values for f including ghost cells:\n");
+
+        for (int r = 0; r < size; r++) {
+            printf("Rank %d: ", r);
+            for (i = 0; i < nxn_loc; i++) {
+                printf("%f ", all_f[r * nxn_loc + i]);
+            }
+            printf("\n");
+        }
+
+        printf("\n");
+        free(all_f);
+    }
+
+    MPI_Finalize();
+}
